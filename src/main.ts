cfa9a5341c356111cc8ad539f41b4afb780720ca
carson <fenimore@gmail.com>
2025-02-05 19:34:10 -1000

    working entity lookup

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,24 @@ import {RackerConfig,
 
 class RackerStacker extends LitElement {
   @property() _config: RackerConfig;
+  @property() _hass: any;
+
   readonly _urlRoot = "/local/racker-stacker";
 
   _models = new Map<string, RackerEquipmentModel>();
   _modelErrors = new Map<string, string>();
+  _entityStates = new Map<string, string>(); // entity_id -> state
+
   static getStubConfig() {
     return {
       name: "rack-1",
       equipment: [],
     };
+  }
+
+  set hass(hass) {
+    this._hass = hass;
+    this.requestUpdate();
   }
 
   setConfig(config: RackerConfig) {
@@ -54,6 +63,7 @@ class RackerStacker extends LitElement {
 	}
 	return html`<div>${msg}</div>`;
     }
+
     let model = this._models.get(eq.model);
     let width_inches = model.width_inches
     const full_width = 19;
@@ -63,9 +73,26 @@ class RackerStacker extends LitElement {
     let height_pixels = Math.floor( model.rack_u * height_1u );
     let img_type = model?.img_type ? model.img_type : "jpg";
     var model_image = `${this._urlRoot}/models/${eq.model}_${this._config?.facing ? this._config.facing : "front"}.${img_type}`;
+
+    var stateIndicator;
+    if (eq.entity && this._hass){
+        const state = this._hass.states[eq.entity];
+        var stateStr = state ? state.state : "unavailable";
+        //console.log(`Entity ${eq.entity} has state ${stateStr}`);
+	var color;
+	if (stateStr === 'on'){
+		color = "rgba(0,255,0,0.5)";
+	} else {
+		// for now, only color FAILING equipment
+		color = "rgba(255,0,0,0.5)";
+		stateIndicator = html`
+		   <div style="position: absolute; background: ${color}; z-index: 3; width: ${width_pixels}px; height: ${height_pixels}px"></div>
+		`;
+	}
+    }
     return html`
     	<div>
-	   <div style="position: absolute; background: rgba(0,255,0,0.5); z-index: 3; width: ${width_pixels}px; height: ${height_pixels}px"></div>
+	   ${stateIndicator}
 	   <img src="${model_image}" alt style="filter: grayscale(1.0); display: block; width: ${width_pixels}px">
 	</div>`;
   }
